9de4b0452eca0cf119dfc347dcb12e97d5c55724
alisher <alisher.tojimov@yandex.ru>
2022-05-04 21:58:42 +0500

    js-dom

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let id = document.getElementById("age-table");
+console.log(id);
+let label = document.querySelectorAll("label");
+console.log(label);
+let td = document.getElementById("age-list").previousElementSibling
+console.log(td);
+let attrb = document.querySelector('[name="search-person"]')
+console.log(attrb);
+let fsInput = document.querySelector('[value="young"]')
+console.log(fsInput);
+let lsChild = document.querySelector('[name="search-person"]').lastChild.previousSibling
+console.log(lsChild);
